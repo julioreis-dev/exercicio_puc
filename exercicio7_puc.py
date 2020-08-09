84811188d012bf3b33c3904e3af5cee8a7c5dfc3
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-08-09 20:11:58 -0300

    Add files via upload

--- exercicio7_puc.py
+++ exercicio7_puc.py
@@ -1,20 +1,39 @@
+def calcular(lista, tot, num):
+    lista_resultado = []
+    repetidos = lista.count(num)
+    percentual = (repetidos*100)/tot
+    lista_resultado.append(repetidos)
+    lista_resultado.append(percentual)
+    return lista_resultado
+
+
 lista_answ = []
 arg = True
 while arg:
-    answ = input('Tipos de problemas'
+    answ = input('\nTipos de problemas'
                  '\n1 - Necessita de esfera'
                  '\n2 - Necessita de limpeza'
                  '\n3 - Necessita de troca de cabo ou conector'
-                 '\n4 - quebrado ou inutilizado')
+                 '\n4 - quebrado ou inutilizado '
+                 '\n')
     opc = [1, 2, 3, 4]
-    x = answ.isdigit()
     if answ.isdigit():
         answ = int(answ)
         if answ in opc:
             lista_answ.append(answ)
+            arg = True
         elif answ == 0:
             print(lista_answ)
+            total = len(lista_answ)
+            print(f'\nQuantidades de mouses: {total}'
+                  f'\nSituação                                   Quantidade    Percentual'
+                  f'\n1-Necessita de esfera                         {calcular(lista_answ, total, 1)[0]}            {calcular(lista_answ, total, 1)[1]}%'
+                  f'\n2-Necessita de limpeza                        {calcular(lista_answ, total, 2)[0]}            {calcular(lista_answ, total, 2)[1]}%'
+                  f'\n3-Necessita de troca de cabo ou conector      {calcular(lista_answ, total, 3)[0]}            {calcular(lista_answ, total, 3)[1]}%'
+                  f'\n4-quebrado ou inutilizado                     {calcular(lista_answ, total, 4)[0]}            {calcular(lista_answ, total, 4)[1]}%')
+
             arg = False
+        else:
+            print('Opçãp inválida')
     else:
         print('\nValor invalido, tente novamente!!!!')
-
